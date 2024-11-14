9018afe0089740a269416970df7654ba63ec01af
Andreicomia11-12 <andreicomia7@gmail.com>
2024-11-14 23:12:36 +0800

    bckEnd SP1

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,1 +1,10 @@
-const exppress = require('express')
+const app = require('./app')
+
+const dotenv = require('dotenv')
+
+//This code tells server.js where to find the port
+dotenv.config({path: 'BackEnd/config/config.env'})
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Listening on Port: ${process.env.PORT} and currently in ${process.env.NODE_ENV}`)
+})
