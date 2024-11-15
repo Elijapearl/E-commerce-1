37319e23f3742d70d2582cb1622740b9db518482
Andreicomia11-12 <andreicomia7@gmail.com>
2024-11-15 09:29:51 +0800

    This is the first savepoint of our backend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const dotenv = require('dotenv')
 
 //This code tells server.js where to find the port
-dotenv.config({path: 'BackEnd/config/config.env'})
+dotenv.config({path: 'BackEnd/config/.env'})
 
 app.listen(process.env.PORT, () =>{
     console.log(`Listening on Port: ${process.env.PORT} and currently in ${process.env.NODE_ENV}`)
