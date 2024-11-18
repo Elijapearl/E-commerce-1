0f5154bef08e233fe9a304f8610510efe8d78a36
FranzCiriaco <franzciriaco1@gmail.com>
2024-11-18 16:37:53 +0800

    reset password

--- BackEnd/models/users.js
+++ BackEnd/models/users.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');//built in node package
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -74,6 +75,20 @@ userSchema.methods.getJwtToken = function (){
     return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_TIME
     })
+}
+
+//Generate password reset token
+
+userSchema.methods.getResetPasswordTojen = function (){
+    //Generate a token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    //Hash and set to reset password
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+    //set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 600 * 1000;
+    return resetToken;
 }
 
 module.exports = mongoose.model('User', userSchema);
